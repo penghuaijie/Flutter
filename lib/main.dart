8edf0105167e7940d0fd634a43bda36908a3ede0
彭怀杰 <penghuaijie@xiaozhu.com>
2019-06-19 20:12:14 +0800

    PD |

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_app/test/carHomePage.dart';
 import 'package:flutter_color_plugin/flutter_color_plugin.dart';
+import 'statelessGroupPage.dart';
 
 void main() => runApp(App());
 
@@ -8,9 +10,14 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: CarHomePage(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('PHJFlutter'),
+        ),
+        body: StatelessGroupPage(),
+      ),
       theme: ThemeData(
-        primaryColor: ColorUtil.color('#f2f2f2'),
+        primaryColor: ColorUtil.color('#ff4081'),
       ),
     );
   }
---
+++ lib/statelessGroupPage.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_color_plugin/flutter_color_plugin.dart';
+/*
+* Container
+* Text
+* Icon
+* CloseButton
+* BackButton
+* Chip
+* Divider
+* Card
+* AlertDialog
+* */
+
+class StatelessGroupPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Colors.white,
+      ),
+      child: Column(
+        children: <Widget>[
+          Text(
+            'hello world',
+            style: TextStyle(
+              color: ColorUtil.color('#ff4081'),
+              fontSize: 40.0,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          Icon(
+            Icons.android,
+            color: ColorUtil.color('#ff4081'),
+            size: 60,
+          ),
+          CloseButton(),
+          BackButton(),
+          Chip(
+            label: Text('penghuaijie'),
+            avatar: Icon(
+              Icons.android,
+            ),
+          ),
+          Divider(
+            color: ColorUtil.color('#ff4081'),
+            height: 10,
+            indent: 20,
+          ),
+          Card(
+            color: ColorUtil.color('#ff4081'),
+            elevation: 5,
+            margin: EdgeInsets.all(10),
+            child: Container(
+              child: Text(
+                'i love you',
+                style: TextStyle(
+//                  color: ColorUtil.color('#ff4081'),
+                  fontSize: 40.0,
+                ),
+              ),
+            ),
+          ),
+          AlertDialog(
+            title: Text('WJM'),
+            content: Text('I love you'),
+          )
+        ],
+      ),
+    );
+  }
+}
